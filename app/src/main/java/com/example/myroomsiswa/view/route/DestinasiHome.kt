71df19ba238044807fc54e48941df00f5278b401
ZhilPro27 <zhilalkrisna@gmail.com>
2026-02-02 02:14:08 +0700

    Mendefinisikan object DestinasiHome pada file DestinasiHome.kt

--- app/src/main/java/com/example/myroomsiswa/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/myroomsiswa/view/route/DestinasiHome.kt
@@ -1,4 +1,9 @@
 package com.example.myroomsiswa.view.route
 
-object DestinasiHome {
+import com.example.myroomsiswa.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+
+    override val titleRes = R.string.app_name
 }
