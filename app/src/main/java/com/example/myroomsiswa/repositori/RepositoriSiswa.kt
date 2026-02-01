e775cd6a75d4b3bc68c6254603a3b9f9b8312f79
ZhilPro27 <zhilalkrisna@gmail.com>
2026-02-02 02:09:31 +0700

    mendefinisikan interface RepositoriSiswa dan class OfflineReposistoriSiswa pada file RepositoriSiswa.kt

--- app/src/main/java/com/example/myroomsiswa/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/myroomsiswa/repositori/RepositoriSiswa.kt
@@ -1,4 +1,20 @@
 package com.example.myroomsiswa.repositori
 
+import com.example.myroomsiswa.room.Siswa
+import com.example.myroomsiswa.room.SiswaDao
+import kotlinx.coroutines.flow.Flow
+
 interface RepositoriSiswa {
+    fun getAllSiswaStream() : Flow<List<Siswa>>
+    suspend fun insertSiswa(siswa: Siswa)
+}
+
+class OfflineRepositoriSiswa(
+    private val siswaDao: SiswaDao
+): RepositoriSiswa {
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao
+        .getAllSiswa()
+
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao
+        .insert(siswa)
 }
