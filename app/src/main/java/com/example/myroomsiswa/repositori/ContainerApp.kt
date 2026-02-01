5e8a301bbbc8370636912c927667f12fcb170c84
ZhilPro27 <zhilalkrisna@gmail.com>
2026-02-02 02:10:54 +0700

    mendefinisikan interface ContainerApp, class ContainerDataApp dan class AplikasiSiswa pada file ContainerApp.kt

--- app/src/main/java/com/example/myroomsiswa/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/myroomsiswa/repositori/ContainerApp.kt
@@ -1,2 +1,32 @@
 package com.example.myroomsiswa.repositori
 
+import android.app.Application
+import android.content.Context
+import com.example.myroomsiswa.room.DatabaseSiswa
+
+
+interface ContainerApp {
+    val repositoriSiswa : RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context):
+    ContainerApp {
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            DatabaseSiswa.getDatabase(context).siswaDao())
+    }
+}
+
+class AplikasiSiswa : Application() {
+    /**
+     * AppContainer instance digunakan oleh kelas-kelas lainnya
+     * untuk mendapatkan dependesi
+     */
+
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
+}
