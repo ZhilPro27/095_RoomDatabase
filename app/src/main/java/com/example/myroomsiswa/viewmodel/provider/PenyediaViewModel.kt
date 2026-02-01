e436d69a0d272d8e6c5425593d5b8efd26fa0918
ZhilPro27 <zhilalkrisna@gmail.com>
2026-02-02 02:25:46 +0700

    Mendefinisikan object PenyediaViewModel pada file PenyediaViewModel.kt

--- app/src/main/java/com/example/myroomsiswa/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myroomsiswa/viewmodel/provider/PenyediaViewModel.kt
@@ -1,4 +1,29 @@
 package com.example.myroomsiswa.viewmodel.provider
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.myroomsiswa.repositori.AplikasiSiswa
+import com.example.myroomsiswa.viewmodel.EntryViewModel
+import com.example.myroomsiswa.viewmodel.HomeViewModel
+
 object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
 }
+
+/**
+ * Fungsi ekstensi query untuk objek [Application] dan mengembalikan sebuah instance dari [ApllikasiSiswa].
+ */
+
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory
+        .APPLICATION_KEY] as AplikasiSiswa)
